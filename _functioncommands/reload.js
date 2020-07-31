b5b8657902424daab2f6642a3129590c3cbdfaec
yttriumtth <62019436+yttriumtth@users.noreply.github.com>
2020-07-31 14:31:30 -0400

    Update reload.js

--- _functioncommands/reload.js
+++ _functioncommands/reload.js
@@ -3,8 +3,13 @@ module.exports = {
 	description: 'Reloads all functions',
 	args: false,
 	execute(message, args, admrole) {
+		const Discord = require('discord.js');
 		if (admrole) {
-			client.destroy();
-		}
+			message.channel.send('Restarting...').then(m => {
+				client.destroy().then(() => {
+					client.login('token');
+				});
+			});
+		} else {message.channel.send('Only Jennymasters can ask of that.')}
 	},
 };
