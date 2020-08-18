501f75fbc1260847f0257a7e2b9d728f1bd76194
yttriumtth <62019436+yttriumtth@users.noreply.github.com>
2020-08-18 16:31:42 -0400

    ghjh

---
+++ _greentextcommands/mandag.js
@@ -0,0 +1,22 @@
+module.exports = {
+	name: 'mandag',
+	aliases: ['dag', 'monday'],
+	execute(message, args) {
+		const imagelist = './imagelists/mandag.txt'; //location of images
+		const framecolor = '#000000'; //personalized frame color
+		const fs = require('fs');
+		const Discord = require('discord.js');
+		fs.readFile(imagelist, (err, data) => {
+			if (err) {
+				console.error(err)
+				return
+			}
+			linesGot = data.toString().split("\n");
+			var randInd = Math.floor(Math.random() * linesGot.length);
+			const imageEmbed = new Discord.RichEmbed()
+			.setColor(framecolor)
+			.setImage(linesGot[randInd])
+			message.channel.send(imageEmbed);
+		})
+	},
+};
