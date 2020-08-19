43d0f5769e00dd94b0d9712c9b0e51cf49a6bd5f
yttriumtth <62019436+yttriumtth@users.noreply.github.com>
2020-08-19 17:11:29 -0400

    Update about.js

--- _functioncommands/about.js
+++ _functioncommands/about.js
@@ -10,13 +10,12 @@ module.exports = {
 		.setTitle('Jenny')
 		.setURL('https://github.com/yttriumtth/jennybotrepo')
 		.setAuthor('The Friend Society', 'https://cdn.discordapp.com/icons/212320806473039873/a_66402aa927dbb05de9af61340536cf1b.webp', 'http://takethehint.net/')
-		.setDescription('Discord Shitpost Bot')
+		.setDescription('Open Source Discord shitpost bot. Visit https://github.com/yttriumtth/jennybotrepo and steal it there.')
 		.setThumbnail('https://cdn.discordapp.com/attachments/535406784085622793/745739489267155124/canvas2.png')
 		.addField('Functions (%)', 'about, haiku, ping', true)
 		.addField('Macros (>)', 'aran, buddy, cozy, dab, eckse, faf, furry, id, jenny, laugh, leoian, mandag, marry, neon, neversoft, starfyter, vivi, waismuth, wrong', true)
 		.addField('Roleplay (.)', 'pet, suicide', true)
-		.setTimestamp()
-		.setFooter('If you know her wherabouts, there is a 500,000,000 spaceshekel bounty. Contact your local space starbucks for more details.');
+		.setFooter('If you know her whereabouts, there is a 500,000,000 Spaceshekel bounty. Contact your local space Starbucks for more details.');
 		return message.channel.send(helpEmbed);
 	},
 };
