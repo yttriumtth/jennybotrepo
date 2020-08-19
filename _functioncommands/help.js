f9dbb2b400e8323d285db85a049314e08537e4f2
yttriumtth <62019436+yttriumtth@users.noreply.github.com>
2020-08-19 11:54:09 -0400

    Delete help.js

--- _functioncommands/help.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-	name: 'help',
-	aliases: ['halp', 'jenny'],
-	description: 'User help',
-	args: false,
-	execute(message, args, admrole) {
-		message.channel.send(fucommands.map(fucommand => fucommand.name).join(', '));
-	},
-};
