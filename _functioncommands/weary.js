3aba13e8abc35db891d4bac38eb571804024eb1c
yttriumtth <62019436+yttriumtth@users.noreply.github.com>
2020-09-03 14:07:51 -0400

    Update weary.js

--- _functioncommands/weary.js
+++ _functioncommands/weary.js
@@ -10,6 +10,9 @@ module.exports = {
 			.setColor('#ff0040')
 			.setImage('https://cdn.discordapp.com/attachments/535406784085622793/751138242903932988/bonfire.gif')
 		message.channel.send(imageEmbed);
-		return message.channel.sendFile('./misc/stagger.mp3');
+		message.channel.send({
+			files: ['./misc/stagger.mp3']
+		});
+		return
 	},
 };
