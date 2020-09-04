9d69b98102822ca680df991c7ed7a31c88417530
yttriumtth <62019436+yttriumtth@users.noreply.github.com>
2020-09-04 10:43:18 -0400

    try

--- _functioncommands/weary.js
+++ _functioncommands/weary.js
@@ -5,17 +5,29 @@ module.exports = {
 	args: false,
 	execute(message, args, admrole) {
 		const Discord = require('discord.js');
-		const imageEmbed = new Discord.RichEmbed()
-			.setColor('#ff0040')
-			.setAuthor('You seem tired; Come, rest here for a while', 'https://cdn.discordapp.com/avatars/283946528090095619/72e70b359da91c5379fea674ded55933.png')
-			.setImage('https://cdn.discordapp.com/attachments/535406784085622793/751138242903932988/bonfire.gif')
-		message.channel.send(imageEmbed);
-		message.channel.send({ 
-			files: [{
-				attachment: './misc/stagger.mp3'
-			}]
+		const fs = require('fs');
+		
+		fs.readFile('./textlists/weary.txt', (err, data) => {
+			if (err) {
+				console.error(err)
+				return
+			}
+			linesGot = data.toString().split("\n");
+			var randSoInd = Math.floor(Math.random() * (linesGot.length / 2));
+			var randLiInd = linesGot.length - randImInd - 1;
+			const randLine = linesGot[randLiInd];
+			const imageEmbed = new Discord.RichEmbed()
+				.setColor('#ff0040')
+				.setAuthor(linesGot[randImInd], 'https://cdn.discordapp.com/avatars/283946528090095619/72e70b359da91c5379fea674ded55933.png')
+				.setImage('https://cdn.discordapp.com/attachments/535406784085622793/751138242903932988/bonfire.gif')
+			message.channel.send(imageEmbed);
+			message.channel.send({ 
+				files: [{
+					attachment: linesGot[randSoInd]
+				}]
+			})
+				.catch(console.error);
+			return
 		})
-			.catch(console.error);
-		return
 	},
 };
