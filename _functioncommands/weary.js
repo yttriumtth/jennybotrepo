619786b7e161beab59369166adffe65e52d343d2
yttriumtth <62019436+yttriumtth@users.noreply.github.com>
2020-09-04 09:02:15 -0400

    Update weary.js

--- _functioncommands/weary.js
+++ _functioncommands/weary.js
@@ -5,9 +5,9 @@ module.exports = {
 	args: false,
 	execute(message, args, admrole) {
 		const Discord = require('discord.js');
-		message.channel.send('You seem tired; Come, rest here for a while');
 		const imageEmbed = new Discord.RichEmbed()
 			.setColor('#ff0040')
+			.setDescription('You seem tired; Come, rest here for a while')
 			.setImage('https://cdn.discordapp.com/attachments/535406784085622793/751138242903932988/bonfire.gif')
 		message.channel.send(imageEmbed);
 		message.channel.send({ 
